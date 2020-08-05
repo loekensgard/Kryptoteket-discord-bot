fc61b78da13aa4bc8257742fc942d826c5577c04
Thorstein Løkensgard / Intility AS <thorstein.lokensgard@intility.no>
2020-08-05 10:34:10 +0200

    added help command

---
+++ Kryptoteket.Bot/Modules/DefaultCommands.cs
@@ -0,0 +1,29 @@
+﻿using Discord.Commands;
+using Kryptoteket.Bot.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kryptoteket.Bot.Modules
+{
+    [Name("DefaultCommands")]
+    public class DefaultCommands : ModuleBase<SocketCommandContext>
+    {
+        private readonly EmbedService _embedService;
+
+        public DefaultCommands(EmbedService embedService)
+        {
+            _embedService = embedService;
+        }
+
+        [Command("help", RunMode = RunMode.Async)]
+        [Summary("Get help")]
+        public async Task GetHelpText()
+        {
+            await ReplyAsync(null, false, _embedService.Embedhelp().Build());
+        }
+
+
+    }
+}
--- Kryptoteket.Bot/Services/EmbedService.cs
+++ Kryptoteket.Bot/Services/EmbedService.cs
@@ -2,6 +2,7 @@
 using Kryptoteket.Bot.Models;
 using System;
 using System.Globalization;
+using System.Text;
 
 namespace Kryptoteket.Bot.Services
 {
@@ -31,6 +32,21 @@ namespace Kryptoteket.Bot.Services
             builder.AddField("Change", $"{price.Change}%");
             builder.WithTimestamp(DateTime.Now);
             builder.WithColor(Color.DarkBlue);
+            return builder;
+        }
+
+        public EmbedBuilder Embedhelp()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Search ticker: !ticker <pair>");
+            sb.AppendLine("Search price: !price <pair>");
+            
+            builder.WithTitle($"Commands");
+            builder.WithDescription(sb.ToString());
+            builder.WithColor(Color.DarkBlue);
+            builder.WithFooter("Bot improvements can be featured to Thorshi#6851");
             return builder;
         }
 
